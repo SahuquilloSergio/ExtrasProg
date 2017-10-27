a0b4197f1c7b49da02545ab8df2b98e5bcf021fd
serxa <serxa@192.168.0.70>
2017-10-27 12:05:59 +0200

    Inicio Proyecto

---
+++ src/tienda/Tienda.java
@@ -0,0 +1,22 @@
+package tienda;
+public class Tienda {
+
+    public static void main(String[] args) {
+
+        Producto Producto1 = new Producto("C1", 24);
+        System.out.println(Producto1.getCodigo()+"   " + Producto1.getPrecio());
+        
+        Producto2 Producto2 = new Producto2("H23", 243);
+        System.out.println(Producto2.toString());
+        
+        Producto Producto3 = new Producto("M30", 109);
+        System.out.println(Producto3.getCodigo()+"   "+Producto3.getPrecio());
+
+        Producto2.cambiarCodigo("K123");
+        Producto2.cambiarPrecio(109);
+        System.out.println(Producto2.toString());
+        System.out.println("MIRA NINA QUE TE JODAN EL ENUNCIADO NO TIENE SENTIDO");
+        
+    }
+}
+ 
